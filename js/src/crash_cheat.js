7ee503da299151bb87b51dbd941a39a62d1368d9
jiang-yuntao <2727171436.qq@gmail.com>
2019-10-15 17:19:41 +0800

    Site updated: 2019-10-15 17:19:36

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,3 @@
+// build time:Tue Oct 15 2019 17:19:27 GMT+0800 (GMT+08:00)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
